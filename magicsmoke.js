11414c8becba4b81a60a30dc82359e9823c1f7c8
KyojuHashira <139628002+KyojuHashira@users.noreply.github.com>
2023-08-05 00:13:10 -0500

    The drawing algorithm has been redesigned
    Fade-out method still has tangible load on GPU

--- magicsmoke.js
+++ magicsmoke.js
@@ -1,28 +1,39 @@
 const smokeEffect = document.getElementById('smoke-effect');
 
+function getRandomGoldColor() {
+  // let's generate a random color close to #deb940 with increased contrast and brightness
+  const red = 222 + Math.random() * 10; // increase red for brightness
+  const green = 185 + Math.random() * 10; // increase green for brightness
+  const blue = 64 - Math.random() * 10; // reduce blue for contrast
+  return `rgba(${red}, ${green}, ${blue}, 1)`;
+}
+
 function getRandomColor() {
   // let's generate a random color close to #2b2b45
-  const red = 43 + Math.random() * 5; // random red component between 43 and 48
-  const green = 43 + Math.random() * 5; // random green component between 43 and 48
-  const blue = 69 + Math.random() * 5; // random blue component between 69 and 74
+  const red = 43 + Math.random() * 2.5;
+  const green = 43 + Math.random() * 2.5;
+  const blue = 69 + Math.random() * 2.5;
   return `rgba(${red}, ${green}, ${blue}, 1)`;
 }
 
 function createRandomStar() {
   const star = document.createElement('div');
   star.className = 'smoke-star';
-  const randomSize = Math.random() * 6 + 2; // random size
+  const randomSize = Math.random() * 9 + 7; // random size between 7 and 15 pixels
   star.style.width = `${randomSize}px`;
   star.style.height = `${randomSize}px`;
   const randomOpacity = Math.random() * 0.5 + 0.2; // random opacity
   star.style.opacity = randomOpacity;
 
-  // 1% of elements brighter
+  // 1% chance for brighter and whiter particle
   if (Math.random() < 0.01) {
     const brightFactor = 0.7 + Math.random() * 0.3; // adjust the brightness here
     star.style.backgroundColor = `rgba(255, 255, 255, ${brightFactor})`;
+  } else if (Math.random() < 0.005) { // 0.5% chance for gold particle
+    star.style.backgroundColor = getRandomGoldColor();
+    star.style.opacity = 1; // always high brightness for gold particles
   } else {
-  // colorize 15% of the elements - something close to the #2b2b45
+    // colorize 15% of the elements (#2b2b45 stars)
     if (Math.random() < 0.15) {
       star.style.backgroundColor = getRandomColor();
     }
@@ -30,27 +41,35 @@ function createRandomStar() {
   return star;
 }
 
+let lastCreationTime = performance.now();
+const CREATION_INTERVAL = 100; // set interval
+
 window.addEventListener('mousemove', (e) => {
   const container = document.querySelector('.circular-widget');
   const containerRect = container.getBoundingClientRect();
   const x = e.clientX - containerRect.left;
   const y = e.clientY - containerRect.top;
 
-  for (let i = 0; i < 7; i++) { // generate "smoke" with stars particles
-    const smokeParticle = createRandomStar();
-    const randomX = x + Math.random() * 100 - 50; // random x position
-    const randomY = y + Math.random() * 50 - 25; // random y position
-    smokeParticle.style.top = `${randomY}px`;
-    smokeParticle.style.left = `${randomX}px`;
-    smokeEffect.appendChild(smokeParticle);
+  const currentTime = performance.now();
+  if (currentTime - lastCreationTime >= CREATION_INTERVAL) {
+    const numberOfParticles = Math.floor(Math.random() * 3) + 3; // random stars amount between 3 and 5
+    for (let i = 0; i < numberOfParticles; i++) {
+      const smokeParticle = createRandomStar();
+      const randomX = x + Math.random() * 60 - 30; // random x position
+      const randomY = y + Math.random() * 60 - 30; // random y position
+      smokeParticle.style.top = `${randomY}px`;
+      smokeParticle.style.left = `${randomX}px`;
+      smokeEffect.appendChild(smokeParticle);
 
-    setTimeout(() => {
-      smokeParticle.style.transition = 'opacity 0.5s ease';
-      smokeParticle.style.opacity = 0;
-    }, 10000); // 10 secs visible, then fade-out
+      setTimeout(() => {
+        smokeParticle.style.transition = 'opacity 0.5s ease';
+        smokeParticle.style.opacity = 0;
+      }, 10000); // 10 secs visible, then fade-out
 
-    setTimeout(() => {
-      smokeParticle.remove();
-    }, 10500); // 10500 - 10000 = 0.5s fade-out
+      setTimeout(() => {
+        smokeParticle.remove();
+      }, 10500); // 10500 - 10000 = 0.5s fade-out
+    }
+    lastCreationTime = currentTime;
   }
 });

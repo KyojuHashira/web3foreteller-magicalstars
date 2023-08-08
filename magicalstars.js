1ce57727d34ea24d55b06fd98c880ff39dee23e3
KyojuHashira <139628002+KyojuHashira@users.noreply.github.com>
2023-08-07 23:45:43 -0500

    Update and rename

--- magicalstars.js
+++ magicalstars.js
@@ -1,5 +1,4 @@
-// if you have any issues, welcome to https://github.com/KyojuHashira/traileffect/issues
-const smokeEffect = document.getElementById('smoke-effect');
+const starsEffect = document.getElementById('stars-effect');
 
 function getRandomGoldColor() {
   // let's generate a random color close to #deb940 with increased contrast and brightness
@@ -19,20 +18,20 @@ function getRandomColor() {
 
 function createRandomStar() {
   const star = document.createElement('div');
-  star.className = 'smoke-star';
-  const randomSize = Math.random() * 9 + 7; // random star size between 7 and 15 pixels
+  star.className = 'magic-star';
+  const randomSize = Math.random() * 9 + 7; // random size between 7 and 15 pixels
   star.style.width = `${randomSize}px`;
   star.style.height = `${randomSize}px`;
-  const randomOpacity = Math.random() * 0.5 + 0.2; // random star opacity
+  const randomOpacity = Math.random() * 0.5 + 0.2; // random opacity
   star.style.opacity = randomOpacity;
 
   // 1% chance for brighter and whiter particle
   if (Math.random() < 0.01) {
     const brightFactor = 0.7 + Math.random() * 0.3; // adjust the brightness here
     star.style.backgroundColor = `rgba(255, 255, 255, ${brightFactor})`;
-  } else if (Math.random() < 0.005) { // set 0.5% chance for gold star
+  } else if (Math.random() < 0.005) { // 0.5% chance for gold particle
     star.style.backgroundColor = getRandomGoldColor();
-    star.style.opacity = 1; // always high brightness for gold star
+    star.style.opacity = 1; // always high brightness for gold particles
   } else {
     // colorize 15% of the elements (#2b2b45 stars)
     if (Math.random() < 0.15) {
@@ -43,7 +42,7 @@ function createRandomStar() {
 }
 
 let lastCreationTime = performance.now();
-const CREATION_INTERVAL = 100; // set interval (the higher the value, the fewer resources are required, and the less frequent the drawing of a random set of stars ranging from 3 to 5)
+const CREATION_INTERVAL = 100; // set interval
 
 window.addEventListener('mousemove', (e) => {
   const container = document.querySelector('.circular-widget');
@@ -55,20 +54,20 @@ window.addEventListener('mousemove', (e) => {
   if (currentTime - lastCreationTime >= CREATION_INTERVAL) {
     const numberOfParticles = Math.floor(Math.random() * 3) + 3; // random stars amount between 3 and 5
     for (let i = 0; i < numberOfParticles; i++) {
-      const smokeParticle = createRandomStar();
-      const randomX = x + Math.random() * 60 - 30; // random star x position
-      const randomY = y + Math.random() * 60 - 30; // random star y position
-      smokeParticle.style.top = `${randomY}px`;
-      smokeParticle.style.left = `${randomX}px`;
-      smokeEffect.appendChild(smokeParticle);
+      const starsParticle = createRandomStar();
+      const randomX = x + Math.random() * 60 - 30; // random x position
+      const randomY = y + Math.random() * 60 - 30; // random y position
+      starsParticle.style.top = `${randomY}px`;
+      starsParticle.style.left = `${randomX}px`;
+      starsEffect.appendChild(starsParticle);
 
       setTimeout(() => {
-        smokeParticle.style.transition = 'opacity 0.5s ease';
-        smokeParticle.style.opacity = 0;
+        starsParticle.style.transition = 'opacity 0.5s ease';
+        starsParticle.style.opacity = 0;
       }, 10000); // 10 secs visible, then fade-out
 
       setTimeout(() => {
-        smokeParticle.remove();
+        starsParticle.remove();
       }, 10500); // 10500 - 10000 = 0.5s fade-out
     }
     lastCreationTime = currentTime;
